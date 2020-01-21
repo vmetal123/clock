63339ce942a3839fed7f3ab476e404eafbe94864
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2020-01-21 00:37:11 +0000

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,12 +4,12 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets\FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "725c948a4cb283f4d91d8ccd10ddc234",
+"/assets\LICENSE": "f799fcf3bb8ef644c1d4cfa24b4510f8",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "ff265a90bb52f87238416a6619e8714a",
-"/main.dart.js": "b128e06f706e60c0494a9bcf7b1311af",
+"/main.dart.js": "994fec6d671788e7b019d9b2ce97646f",
 "/manifest.json": "2f2e61c650e822bbf97f4c70825af2cf"
 };
 
